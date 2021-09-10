c6ec39719f2b74e7e6bb1b04263805b3521ff228
Stephanie <ishnewalon@hotmail.com>
2021-09-10 16:57:55 -0400

    Initialisation du projet

---
+++ src/main/java/com/gestionnaire_de_stage/GestionnaireDeStageApplication.java
@@ -0,0 +1,13 @@
+package com.gestionnaire_de_stage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionnaireDeStageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionnaireDeStageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gestionnaire_de_stage/GestionnaireDeStageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionnaire_de_stage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionnaireDeStageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
