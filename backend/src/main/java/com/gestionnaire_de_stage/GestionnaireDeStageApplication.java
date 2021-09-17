d390e38649a3176122dce96133898006ffa48f32
Slim Crisp <jordan.scott1396@gmail.com>
2021-09-17 16:36:05 -0400

    Ajout component scan

--- backend/src/main/java/com/gestionnaire_de_stage/GestionnaireDeStageApplication.java
+++ backend/src/main/java/com/gestionnaire_de_stage/GestionnaireDeStageApplication.java
@@ -2,8 +2,10 @@ package com.gestionnaire_de_stage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.gestionnaire_de_stage.*")
 public class GestionnaireDeStageApplication {
 
     public static void main(String[] args) {
