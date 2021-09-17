793ce12cfbcb2ad1e8dbc9473e09efb94feca40b
Imem Belcadi <belcadiimem01@gmail.com>
2021-09-17 15:37:09 -0400

    Ajout Page Password

--- gestionnaire_de_stage_frontend/src/components/Register/Register.js
+++ gestionnaire_de_stage_frontend/src/components/Register/Register.js
@@ -44,6 +44,7 @@ export class Register extends Component {
         } = this.state;
         const values = {email, password, first_name, last_name,phone}
         const valMoniteur = {companyName, city, address, codePostal}
+        const valPwd = {password}
         let show = null;
         switch (step) {
             case 1:
@@ -53,6 +54,10 @@ export class Register extends Component {
             case 2:
                 show = <Moniteur prevStep={this.prevStep} nextStep={this.nextStep} handleChange={this.handleChange}
                                  values={valMoniteur}/>
+                break;
+            case 3:
+                show = <PwdPart prevStep={this.prevStep} nextStep={this.nextStep} handleChange={this.handleChange}
+                            values={valPwd}/>
                 break;
             default:
                 break;
@@ -184,6 +189,73 @@ const Moniteur = ({prevStep, nextStep, handleChange, values}) => {
                 <label/>
                 <div>
                     <button className="btn btn-primary" type={"button"} onClick={Previous}>Precedent</button>
+                    <button className="btn btn-primary" type={"button"} onClick={Continue}>Suivant</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const PwdPart = ({prevStep, nextStep, handleChange, values}) => {
+
+    const Previous = e => {
+        e.preventDefault();
+        prevStep();
+    }
+    const Continue = e => {
+        e.preventDefault();
+        nextStep();
+    }
+    return (<div>
+                <div className="form-group row">
+                    <div className="col-md-12">
+                        <label>Mot de passe</label>
+                        <div className="input-group">
+                            <input name="pwd" placeholder="Votre mot de passe" className="form-control" type="password"
+                                   value={values.password} onChange={handleChange('password')}/>
+                        </div>
+                    </div>
+
+                    {/*TODO REVOIR LA CONFIRMATION DU MDP*/}
+                    {/*<div className="col-md-6">*/}
+                    {/*    <label>Confirmez votre mot de passe</label>*/}
+                    {/*    <div className="input-group">*/}
+                    {/*        <input name="pwd2" placeholder="Confirmez votre mot de passe" className="form-control" type="text"*/}
+                    {/*               value={values.pwd2} onChange={handleChange('pwd2')}/>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
+                    <div className="form-group text-center">
+                        <label/>
+                        <div>
+                            <button className="btn btn-primary" type={"button"} onClick={Previous}>Precedent</button>
+                            <button className="btn btn-primary" type={"button"} onClick={Continue}>Suivant</button>
+                        </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const Cegep = ({nextStep, handleChange, values}) => {
+
+    const Continue = e => {
+        e.preventDefault();
+        nextStep();
+    }
+
+    return (<div>
+            <div className="form-group row">
+                <div className="col-md-6">
+                    <label>Matricule</label>
+                    <div className="input-group">
+                        <input name="matricule" placeholder="Matricule" className="form-control" type="text"
+                               value={values.matricule} onChange={handleChange('matricule')}/>
+                    </div>
+                </div>
+            </div>
+            <div className="form-group text-center">
+                <label/>
+                <div>
                     <button className="btn btn-primary" type={"button"} onClick={Continue}>Suivant</button>
                 </div>
             </div>
