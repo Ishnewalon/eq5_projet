1e9b0e8e5033ca88743187cc3f2ee9f94c9d3f78
Stephanie <ishnewalon@hotmail.com>
2021-09-22 11:59:13 -0400

    Creation du repository SupervisorRepository

---
+++ backend/src/main/java/com/gestionnaire_de_stage/repository/SupervisorRepository.java
@@ -0,0 +1,9 @@
+package com.gestionnaire_de_stage.repository;
+
+import com.gestionnaire_de_stage.model.Supervisor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupervisorRepository extends JpaRepository<Supervisor, Long> {
+}
