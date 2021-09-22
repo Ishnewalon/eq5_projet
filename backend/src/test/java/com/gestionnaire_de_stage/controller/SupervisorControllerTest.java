9d3fbc7b866f134190c6589a975a95b560da7f20
Stephanie <ishnewalon@hotmail.com>
2021-09-22 12:13:53 -0400

    Creation du test SupervisorControllerTest

---
+++ backend/src/test/java/com/gestionnaire_de_stage/controller/SupervisorControllerTest.java
@@ -0,0 +1,9 @@
+package com.gestionnaire_de_stage.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(SupervisorController.class)
+@AutoConfigureMockMvc
+public class SupervisorControllerTest {
+}
