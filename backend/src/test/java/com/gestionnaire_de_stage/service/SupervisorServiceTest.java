23faf0452934647e0fd1a7639e4fc06d63dbe3d7
Stephanie <ishnewalon@hotmail.com>
2021-09-22 12:10:57 -0400

    Creation du test SupervisorServiceTest

---
+++ backend/src/test/java/com/gestionnaire_de_stage/service/SupervisorServiceTest.java
@@ -0,0 +1,9 @@
+package com.gestionnaire_de_stage.service;
+
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class SupervisorServiceTest {
+}
