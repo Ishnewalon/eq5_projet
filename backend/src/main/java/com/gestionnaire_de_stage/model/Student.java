ae1f8bc8e535655c6d0cd7d69f93ef95a89c886d
Stephanie <ishnewalon@hotmail.com>
2021-09-23 10:07:37 -0400

    Ajout de la longueur des matricule pour Supervisor et Student

--- backend/src/main/java/com/gestionnaire_de_stage/model/Student.java
+++ backend/src/main/java/com/gestionnaire_de_stage/model/Student.java
@@ -4,12 +4,14 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @Entity
 public class Student extends User{
 
     @NotNull
+    @Size(min = 7, max = 7, message = "La matricule doit être de 7 chiffre")
     private String matricule;
 
     @NotNull
--- backend/src/main/java/com/gestionnaire_de_stage/model/Supervisor.java
+++ backend/src/main/java/com/gestionnaire_de_stage/model/Supervisor.java
@@ -4,12 +4,14 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @Entity
 public class Supervisor extends User{
 
     @NotNull
+    @Size(min = 5, max = 5, message = "La matricule doit être de 5 chiffre")
     private String matricule;
 
     @NotNull
