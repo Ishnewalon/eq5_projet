a6a6fe5fda70327aef6729e22e61d13a433fcb5e
Stephanie <ishnewalon@hotmail.com>
2021-09-23 10:28:38 -0400

    Ajout de insertData dans SupervisorServiceTest

--- backend/src/test/java/com/gestionnaire_de_stage/service/SupervisorServiceTest.java
+++ backend/src/test/java/com/gestionnaire_de_stage/service/SupervisorServiceTest.java
@@ -1,9 +1,56 @@
 package com.gestionnaire_de_stage.service;
 
+import com.gestionnaire_de_stage.model.Supervisor;
+import com.gestionnaire_de_stage.repository.SupervisorRepository;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@DataJpaTest
+import java.util.Arrays;
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = JpaRepository.class))
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SupervisorServiceTest {
+
+    @Autowired
+    SupervisorRepository supervisorRepository;
+
+    @Autowired
+    SupervisorService supervisorService;
+
+    @BeforeAll
+    public void insertData {
+        Supervisor supervisor1 = new Supervisor();
+        supervisor1.setId(1L);
+        supervisor1.setName("Keys");
+        supervisor1.setFirstName("Harold");
+        supervisor1.setEmail("keyh@gmail.com");
+        supervisor1.setPassword("galaxy29");
+        supervisor1.setDepartment("Comptabilité");
+        supervisor1.setMatricule("04736");
+
+        Supervisor supervisor2 = new Supervisor();
+        supervisor2.setId(2L);
+        supervisor2.setName("Hole");
+        supervisor2.setFirstName("Van");
+        supervisor2.setEmail("holv@gmail.com");
+        supervisor2.setPassword("digdigdig");
+        supervisor2.setDepartment("Science Naturelle");
+        supervisor2.setMatricule("47854");
+
+        Supervisor supervisor3 = new Supervisor();
+        supervisor3.setId(3L);
+        supervisor3.setName("Olive");
+        supervisor3.setFirstName("Peter");
+        supervisor3.setEmail("olvp@gmail.com");
+        supervisor3.setPassword("jobgobdob");
+        supervisor3.setDepartment("Informatique");
+        supervisor3.setMatricule("23478");
+
+        supervisorRepository.saveAll(Arrays.asList(supervisor1, supervisor2, supervisor3));
+    }
 }
