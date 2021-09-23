c13193999739f06d6be9f3b636d8812838f39494
Samuel Bertrand <bertsa.pro@gmail.com>
2021-09-23 15:51:37 -0400

    Login
    Co-authored-by: Imem Belcadi Abbassi <belcadiimem01@gmail.com>

--- gestionnaire_de_stage_frontend/src/App.js
+++ gestionnaire_de_stage_frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
     return (
         <div className="container">
             <Register/>
-            {/*<Login/>*/}
+            <Login/>
             {/*<TemplateCustomBS/>*/}
         </div>
 
--- gestionnaire_de_stage_frontend/src/components/Login/Login.js
+++ gestionnaire_de_stage_frontend/src/components/Login/Login.js
@@ -3,33 +3,47 @@ import {Component} from "react";
 
 class Login extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            matricule: '',
+            password: ''
+        }
+    }
 
-render(){
-    return (<div>
-            <div className="form-group row">
-                <div className="col-md-6">
-                    <label>Matricule</label>
-                    <div className="input-group">
-                        <input name="matricule" placeholder="Matricule" className="form-control" type="text"
-                               value={this.props.values.matricule} onChange={this.props.handleChange('matricule')}/>
-                    </div>
-                </div>
-                <div className="col-md-6">
-                    <label>Nom</label>
-                    <div>
+    handleChange = (e) => {
+        e.preventDefault();
+
+    }
+
+    render() {
+        return (<div>
+                <div className="form-group row">
+                    <div className="col-md-6">
+                        <label>Matricule</label>
                         <div className="input-group">
-                            <input name="password" placeholder="Votre mot de passe" className="form-control" type="password"
-                                   value={this.props.values.password} onChange={this.props.handleChange('password')}/>
+                            <input name="matricule" placeholder="Matricule" className="form-control" type="text"
+                                   value={this.state.matricule} onChange={() => this.handleChange('matricule')}/>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <label>Nom</label>
+                        <div>
+                            <div className="input-group">
+                                <input name="password" placeholder="Votre mot de passe" className="form-control"
+                                       type="password"
+                                       value={this.state.password} onChange={() => this.handleChange('password')}/>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="form-group text-center">
-                <label/>
-                    <button className="btn btn-primary" type={"button"} >Connexion</button>
+                <div className="form-group text-center">
+                    <label/>
+                    <button className="btn btn-primary" type={"button"}>Connexion</button>
                 </div>
             </div>
         )
     }
 }
+
 export default Login;
