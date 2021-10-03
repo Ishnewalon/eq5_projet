0b76816c30e43fee491f281ca2e81255487716db
Stephanie <ishnewalon@hotmail.com>
2021-10-03 13:58:23 -0400

    Ajout d'un drag&drop basic dans TeleverserCV component

--- gestionnaire_de_stage_frontend/src/components/TeleverserCv/TeleverserCv.jsx
+++ gestionnaire_de_stage_frontend/src/components/TeleverserCv/TeleverserCv.jsx
@@ -1,14 +1,28 @@
 import './TeleverserCv.css'
 import React, {Component} from "react";
+import Dropzone from "react-dropzone";
+
 
 class TeleverserCv extends Component {
 
+    onDrop = acceptedFiles => {
+        console.log(acceptedFiles)
+    }
 
     render() {
-        return (<div>
 
-            </div>
-        )
+        return (
+            <Dropzone onDrop={this.onDrop} >
+                {({getRootProps, getInputProps, isDragActive}) => (
+                    <div {...getRootProps()}>
+                        <input {...getInputProps()}/>
+                        {isDragActive ? "Déposez votre C.V ici" : "Cliquer moi ou glisser votre C.V. ici"}
+
+
+                    </div>
+                )}
+            </Dropzone>
+        );
     }
 }
 export default TeleverserCv
