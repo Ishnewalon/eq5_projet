3ae0da0bb0bd9e09cef924227e365b69fe42131b
Imem Belcadi Abbassi <belcadiimem01@gmail.com>
2021-10-08 12:25:37 -0400

    commit to merge

--- gestionnaire_de_stage_frontend/src/components/pdf/ValiderCv.jsx
+++ gestionnaire_de_stage_frontend/src/components/pdf/ValiderCv.jsx
@@ -1,5 +1,5 @@
 import './ValiderCv.css'
-import React, {useState} from "react";
+import React, {Component, useState} from "react";
 import {Document, Page} from "react-pdf";
 
 class ValiderCv extends Component {
