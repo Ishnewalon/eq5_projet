5057cf8bb987e84d3e0b52059877ca9f5730d283
Slim Crisp <jordan.scott1396@gmail.com>
2021-10-07 23:47:08 -0400

    Implementation de upload curriculum

--- backend/src/main/java/com/gestionnaire_de_stage/repository/CurriculumRepository.java
+++ backend/src/main/java/com/gestionnaire_de_stage/repository/CurriculumRepository.java
@@ -4,6 +4,10 @@ import com.gestionnaire_de_stage.model.Curriculum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CurriculumRepository extends JpaRepository<Curriculum, Long> {
+
+    List<Curriculum> findAllByValid(boolean valid);
 }
