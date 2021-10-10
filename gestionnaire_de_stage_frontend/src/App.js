e7eb6c6de676645428255aca3897483c652d13e0
OussaKab <oussamakably@gmail.com>
2021-10-10 01:14:00 -0400

    EQ5-10 - FrontEnd

--- gestionnaire_de_stage_frontend/src/App.js
+++ gestionnaire_de_stage_frontend/src/App.js
@@ -6,6 +6,7 @@ import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
+import ViewOffers from "./components/ViewOffers/ViewOffers";
 import React from "react";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -30,11 +31,14 @@ function App() {
                 <li>
                     <Link to="/dashboard">Dash</Link>
                 </li>
+                <li>
+                    <Link to="/view-offers">Voir offres</Link>
+                </li>
             </ul>
         </nav>
         <div className="container">
             <Switch>
-
+                <ProtectedRoute exact path="/view-offers" component={ViewOffers} />
                 <UnprotectedRoute exact path="/login"
                                   component={Login}/>
                 <UnprotectedRoute exact path="/register"
