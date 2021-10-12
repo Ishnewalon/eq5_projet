9ef461428fbde846f695ca0a3cabc4757947df9c
Stephanie <ishnewalon@hotmail.com>
2021-10-12 15:43:20 -0400

    Refactoring de StudentServiceTest

--- backend/src/test/java/com/gestionnaire_de_stage/service/StudentServiceTest.java
+++ backend/src/test/java/com/gestionnaire_de_stage/service/StudentServiceTest.java
@@ -4,72 +4,24 @@ import com.gestionnaire_de_stage.model.Student;
 import com.gestionnaire_de_stage.repository.StudentRepository;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest(includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = JpaRepository.class))
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
 public class StudentServiceTest {
 
-    @Autowired
-    StudentRepository studentRepository;
-
-    @Autowired
+    @InjectMocks
     StudentService studentService;
 
-    @BeforeAll
-    public void insertData() {
-        Student student1 = new Student();
-        student1.setId(1L);
-        student1.setLastName("Scott");
-        student1.setFirstName("Jordan");
-        student1.setEmail("jscotty@gmail.com");
-        student1.setPhone("514-546-2375");
-        student1.setPassword("rockrocks");
-        student1.setAddress("758 George");
-        student1.setCity("LaSalle");
-        student1.setDepartment("Informatique");
-        student1.setPostalCode("H5N 9F2");
-        student1.setMatricule("6473943");
-
-        Student student2 = new Student();
-        student2.setId(2L);
-        student2.setLastName("Jot");
-        student2.setFirstName("Paul");
-        student2.setEmail("pertFaul@gmail.com");
-        student2.setPhone("514-765-8357");
-        student2.setPassword("kald329345");
-        student2.setAddress("961 Lifew");
-        student2.setCity("Verdun");
-        student2.setDepartment("Comptabilite");
-        student2.setPostalCode("K1F R5V");
-        student2.setMatricule("3728243");
-
-        Student student3 = new Student();
-        student3.setId(3L);
-        student3.setLastName("Tremblay");
-        student3.setFirstName("Emily");
-        student3.setEmail("treme@gmail.com");
-        student3.setPhone("514-924-7854");
-        student3.setPassword("tough8475");
-        student3.setAddress("8542 Schevchenko");
-        student3.setCity("LaSalle");
-        student3.setDepartment("Science Humaine");
-        student3.setPostalCode("H3J 1D8");
-        student3.setMatricule("1749305");
-
-        studentRepository.saveAll(Arrays.asList(student1,student2,student3));
-    }
+    @Mock
+    StudentRepository studentRepository;
 
     @Test
     public void testFindAll() {
