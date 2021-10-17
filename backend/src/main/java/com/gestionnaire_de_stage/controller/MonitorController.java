894af8fe1598175f16dbb66c01227add7fc06dc2
Jordan Scott <jordan.scott1396@gmail.com>
2021-10-17 19:15:29 -0400

    Trying to pull

--- backend/src/main/java/com/gestionnaire_de_stage/controller/MonitorController.java
+++ backend/src/main/java/com/gestionnaire_de_stage/controller/MonitorController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 @RestController
 @CrossOrigin
--- backend/src/main/java/com/gestionnaire_de_stage/repository/MonitorRepository.java
+++ backend/src/main/java/com/gestionnaire_de_stage/repository/MonitorRepository.java
@@ -2,7 +2,6 @@ package com.gestionnaire_de_stage.repository;
 
 import com.gestionnaire_de_stage.model.Monitor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface MonitorRepository extends JpaRepository<Monitor, Long> {
 
