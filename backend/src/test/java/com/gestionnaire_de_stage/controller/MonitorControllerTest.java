e878ca326e9023eb2a2bd0d1f0d64c6b8ceacb1b
Imem Belcadi Abbassi <belcadiimem01@gmail.com>
2021-10-17 18:49:42 -0400

    Manager refactor login, + coverage manager 100%

--- backend/src/test/java/com/gestionnaire_de_stage/controller/MonitorControllerTest.java
+++ backend/src/test/java/com/gestionnaire_de_stage/controller/MonitorControllerTest.java
@@ -12,6 +12,7 @@ import com.gestionnaire_de_stage.service.StudentService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
@@ -27,8 +28,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.*;
 
-@AutoConfigureMockMvc
-@SpringBootTest
+@WebMvcTest(MonitorController.class)
 public class MonitorControllerTest {
 
     @Autowired
