4350a0ba4c9ec974ee34aaf67a1e37433ae53693
Imem Belcadi Abbassi <belcadiimem01@gmail.com>
2021-10-26 12:41:40 -0400

    Ajout componenet register

--- Veille/projet_veille/src/App.vue
+++ Veille/projet_veille/src/App.vue
@@ -1,26 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <Register msg="Bienvenue dans la page du l'inscription "/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Register from "./components/Register/Register";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Register
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ Veille/projet_veille/src/components/Register/Register.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <form>
+      <h1> {{ titre }}</h1>
+      <div class="form-group row">
+
+        <div class="col-md-6">
+          <label>Prénom</label>
+          <div class="input-group">
+            <input name="firstName" placeholder="Prenom" class="form-control" type="text"/>
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <label>Nom</label>
+          <div>
+            <div class="input-group">
+              <input name="lastName" placeholder="Nom" class="form-control" type="text"/>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label>E-Mail</label>
+        <div class="input-group">
+          <input name="email" placeholder="Adresse E-mail" class="form-control" type="email"/>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label>Téléphone</label>
+        <div class="input-group">
+          <input name="contact_no" placeholder="000 000 000" class="form-control" type="tel" />
+        </div>
+      </div>
+
+      <div class="form-group text-center">
+        <label/>
+        <div>
+          <button class="btn btn-primary" type="button" >S'inscrire</button>
+        </div>
+      </div>
+
+    </form>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Register',
+  props: {
+    titre: String
+  }
+}
+</script>
+
+
+<style scoped>
+label {
+  margin-top: 20px;
+  margin-bottom: 10px;
+}
+
+button {
+  margin-right: 10px;
+}
+
+</style>
