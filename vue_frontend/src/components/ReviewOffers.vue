b061a1498fdea203fcf4f6523b3b1635f1d90b3a
OussaKab <oussamakably@gmail.com>
2021-10-26 14:03:02 -0400

    Component creations

---
+++ vue_frontend/src/components/ReviewOffers.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <ul>
+      <li v-for="(offer, index) in offers" :key="index">
+        <ValidateOffer :offer="offer"></ValidateOffer>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+
+import axios from "axios";
+
+export default {
+  name: "ReviewOffers",
+  data() {
+    return {
+      offers: []
+    }
+  },
+  created: function(){
+    this.loadOffers();
+  },
+  methods: {
+    loadOffers: function(){
+      axios.get('http://localhost:8181/offers').then(response => {
+        const {message} = response;
+        if(!message)
+          this.offers = response;
+        else
+          alert(response.message);
+      });
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue_frontend/src/components/ValidateOffer.vue
@@ -0,0 +1,26 @@
+<template>
+
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "ValidateOffer",
+  props: ['offer'],
+  data(){
+    return {
+      valid: false
+    }
+  },
+  methods: {
+    validateOffer: function(){
+      axios.post('http://localhost:8181/offers/validate', this.offer).then(response => (this.valid = response));
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
