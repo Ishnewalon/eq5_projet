e8faf34a21fd44f3c600d058796054bf464180ef
OussaKab <oussamakably@gmail.com>
2021-10-27 02:01:52 -0400

    enlèvement du -

--- vue_frontend/src/App.vue
+++ vue_frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div></div>
 </template>
--
+
 <script>
 
 export default {
@@ -12,12 +12,4 @@ export default {
 
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
