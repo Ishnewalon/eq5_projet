1763a8f16f31a3026b30104fb2351e420bd81271
Samuel Bertrand <bertsa.pro@gmail.com>
2021-10-28 12:54:21 -0400

    class to func

--- gestionnaire_de_stage_frontend/src/components/Manager/ValidateOffer/ValidateOffer.jsx
+++ gestionnaire_de_stage_frontend/src/components/Manager/ValidateOffer/ValidateOffer.jsx
@@ -1,49 +1,44 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import PreviewOffer from "../../PreviewOffer/PreviewOffer";
 import OfferService from '../../../services/offer-service';
 import {swalErr} from "../../../utility";
 import Swal from "sweetalert2";
 import "./ValidateOffer.css";
 
-export default class ValidateOffer extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            valid: false
-        }
-        this.service = OfferService
-    }
+export default function ValidateOffer({offer}) {
+    const [valid, setValid] = useState(false);
+    const service = OfferService
 
-    validateOffer = (offer, valid) => {
-        offer.valid = valid;
-        this.service.validateOffer(offer)
+    const validateOffer = (offer, isValid) => {
+        offer.valid = isValid;
+        service.validateOffer(offer)
             .then(v => {
-                if(typeof v === "string") {
-                    console.trace(v);
+                if (typeof v === "string") {
+                    console.error(v);
                     swalErr(v).fire({}).then();
                     return;
                 }
-                this.setState({valid})
-                Swal.fire(valid ? 'Offre validée!' : 'Offre invalidée!', '', valid ? 'success': 'error').then();
+                setValid(isValid)
+                let title = isValid ? 'Offre validée!' : 'Offre invalidée!'
+                Swal.fire({title: title, icon: 'success'}).then();
             })
             .catch(e => {
-                console.trace(e)
+                console.error(e)
                 swalErr(e).fire({}).then();
             });
+
     }
 
-    render() {
-        const {offer} = this.props;
-        const {valid} = this.state;
 
-        return <div className={`${valid ? 'border-left border-success' : 'border-left border-danger'}`}>
-                <PreviewOffer offer={offer}/>
-                <div className="d-flex justify-content-between align-items-center">
-                    <button id="validateBtn" className="btn btn-success fw-bold text-white w-50"
-                            onClick={() => this.validateOffer(offer, true)}>Valide</button>
-                    <button id="invalidateBtn" className="btn btn-danger fw-bold text-white w-50"
-                            onClick={() => this.validateOffer(offer, false)}>Invalide</button>
-                </div>
-            </div>
-    }
+    return <div className={`${valid ? 'border-left border-success' : 'border-left border-danger'}`}>
+        <PreviewOffer offer={offer}/>
+        <div className="d-flex justify-content-between align-items-center">
+            <button id="validateBtn" className="btn btn-success fw-bold text-white w-50"
+                    onClick={() => validateOffer(offer, true)}>Valide
+            </button>
+            <button id="invalidateBtn" className="btn btn-danger fw-bold text-white w-50"
+                    onClick={() => validateOffer(offer, false)}>Invalide
+            </button>
+        </div>
+    </div>
 }
