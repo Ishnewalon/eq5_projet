6d9d4c5eecaba3badb85a636954c8530a0f7b1a7
Samuel Bertrand <bertsa.pro@gmail.com>
2021-11-01 09:33:53 -0400

    100% line coverage

--- gestionnaire_de_stage_frontend/src/components/Register/Steps/StepMonitor.test.jsx
+++ gestionnaire_de_stage_frontend/src/components/Register/Steps/StepMonitor.test.jsx
@@ -29,7 +29,7 @@ afterEach(() => {
     jest.resetAllMocks();
 })
 
-it('loads and displays StepMonitor', () => {
+test('loads and displays StepMonitor', () => {
     expect(screen.getByTestId("companyName")).toBeInTheDocument();
     expect(screen.getByTestId("input-city")).toBeInTheDocument();
     expect(screen.getByTestId("codePostal")).toBeInTheDocument();
@@ -41,7 +41,8 @@ it('loads and displays StepMonitor', () => {
     expect(screen.getByTestId("companyName")).toHaveValue(myCompanyName);
 
 });
-it('click next', () => {
+
+test('click next', () => {
     service.verification = jest.fn(() => {
         return true;
     });
@@ -52,39 +53,51 @@ it('click next', () => {
     expect(mockFnNext).toHaveBeenCalled()
     expect(mockFnUpdateType).toHaveBeenCalled()
 });
-it('click prev', () => {
+
+test('click prev', () => {
     expect(mockFnPrev).not.toHaveBeenCalled()
     userEvent.click(screen.getByText("Précédent"));
 
     expect(mockFnPrev).toHaveBeenCalled()
 });
-it('verification', () => {
+
+test('verification', () => {
     let myBool = verification('myBigCompagnie', 'mtl', '555 rue blabla', 'H0H0H0');
     expect(myBool).toBeTruthy();
 });
-it('verification bad postalCode', () => {
+
+test('verification bad postalCode', () => {
     let myBool = verification('myBigCompagnie', 'mtl', '555 rue blabla', 'H0H0H03');
     expect(myBool).toBeFalsy();
     myBool = verification('myBigCompagnie', 'mtl', '555 rue blabla', 'H0H03H');
     expect(myBool).toBeFalsy();
 });
-it('verification bad companyName', () => {
+
+test('verification bad companyName', () => {
     let myBool = verification('myBigCompagnie!', 'mtl', '555 rue blabla', 'H0H0H0');
     expect(myBool).toBeFalsy();
 });
-it('verification undefined CompanyName', () => {
+test('verification bad city', () => {
+    let myBool = verification('myBigCompagnie', 'mtl1', '555 rue blabla', 'H0H0H0');
+    expect(myBool).toBeFalsy();
+});
+
+test('verification undefined CompanyName', () => {
     let myBool = verification(undefined, 'mtl', '555 rue blabla', 'H0H0H0');
     expect(myBool).toBeFalsy();
 });
-it('verification undefined City', () => {
+
+test('verification undefined City', () => {
     let myBool = verification('myBigCompagnie', undefined, '555 rue blabla', 'H0H0H0');
     expect(myBool).toBeFalsy();
 });
-it('verification undefined Address', () => {
+
+test('verification undefined Address', () => {
     let myBool = verification('myBigCompagnie', 'mtl', undefined, 'H0H0H0');
     expect(myBool).toBeFalsy();
 });
-it('verification undefined PostalCode', () => {
+
+test('verification undefined PostalCode', () => {
     let myBool = verification('myBigCompagnie', 'mtl', '555 rue blabla', undefined);
     expect(myBool).toBeFalsy();
 })
