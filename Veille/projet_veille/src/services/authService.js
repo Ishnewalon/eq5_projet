0542b285709762dbbc4b4ba32a1717a9d33e567b
Imem Belcadi Abbassi <belcadiimem01@gmail.com>
2021-11-01 13:59:02 -0400

    Ajout fonction registerMonitor et registerSupervisor

--- Veille/projet_veille/src/services/authService.js
+++ Veille/projet_veille/src/services/authService.js
@@ -9,3 +9,21 @@ export async function registerStudent(etudiant) {
     })
     return await response.json();
 }
+
+export async function registerMonitor(monitor) {
+    const response = await fetch(`${urlBackend}/monitor/signup`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(monitor)
+    })
+    return await response.json();
+}
+
+export async function registerSupervisor(supervisor) {
+    const response = await fetch(`${urlBackend}/supervisor/signup`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(supervisor)
+    })
+    return await response.json();
+}
