f727d3266bf648e5c4ff4d7eb8891378fcf9e6f0
Stephanie <ishnewalon@hotmail.com>
2021-11-04 22:37:11 -0400

    Removed Repository Annotation in ContractRepository

--- backend/src/main/java/com/gestionnaire_de_stage/repository/ContractRepository.java
+++ backend/src/main/java/com/gestionnaire_de_stage/repository/ContractRepository.java
@@ -2,10 +2,8 @@ package com.gestionnaire_de_stage.repository;
 
 import com.gestionnaire_de_stage.model.Contract;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import java.util.List;
 
-@Repository
 public interface ContractRepository extends JpaRepository<Contract, Long> {
 
     List<Contract> getAllByManagerSignatureNull();
