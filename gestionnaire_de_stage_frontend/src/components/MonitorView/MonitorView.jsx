6e930a2c541c9a2333f9a3d0f63518de24ea45db
OussaKab <oussamakably@gmail.com>
2021-11-11 16:33:53 -0500

    Frontend - Création des components

--- gestionnaire_de_stage_frontend/src/components/MonitorView/MonitorView.jsx
+++ gestionnaire_de_stage_frontend/src/components/MonitorView/MonitorView.jsx
@@ -6,6 +6,7 @@ import ContractsToBeSigned from "../Contract/ContractsToBeSigned";
 import {UserType} from "../../enums/UserTypes";
 import {ContainerBox} from "../SharedComponents/ContainerBox/ContainerBox";
 import {useAuth} from "../../services/use-auth";
+import MonitorVisitForm from "./VisitForm/MonitorVisitForm";
 
 export default function MonitorView() {
     let {path} = useRouteMatch();
@@ -20,6 +21,9 @@ export default function MonitorView() {
         <Route exact path={`${path}/voir/futures_stagiaires`}>
             <h1 className='text-center'>Contrats de futures stagiaires à valider</h1>
             <ContractsToBeSigned userType={UserType.MONITOR[0]}/>
+        </Route>
+        <Route exact path={`${path}/monitor_form_visit_company`}>
+            <MonitorVisitForm/>
         </Route>
         <Route exact path={`${path}`}>
             <h1 className="text-center">Bonjour {auth.user.firstName}!</h1>
