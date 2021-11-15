6de68d29679f423827117f7b9f9d16061a8342ef
Samuel Bertrand <bertsa.pro@gmail.com>
2021-11-14 19:27:47 -0500

    SignIn works!!

--- gestionnaire_de_stage_frontend/src/components/Login/Login.test.jsx
+++ gestionnaire_de_stage_frontend/src/components/Login/Login.test.jsx
@@ -1,2 +1,43 @@
-test.todo('Login component')
-test.todo('connect')
+// noinspection JSCheckFunctionSignatures
+
+import {cleanup, render, screen} from "@testing-library/react";
+import Login from "./Login";
+import userEvent from "@testing-library/user-event";
+import {useAuth} from "../../services/use-auth";
+
+
+
+jest.mock('../SharedComponents/Fields/FieldEmail', () => () => 'myEmailComponents');
+jest.mock('../SharedComponents/Fields/FieldPassword', () => () => 'myPasswordComponents');
+jest.mock('../../services/use-auth')
+beforeEach(() => {
+
+});
+
+afterEach(() => {
+    cleanup();
+    jest.resetAllMocks();
+})
+test('Login component', () => {
+    render(<Login/>);
+
+    expect(screen.getByText("myEmailComponents")).toBeInTheDocument()
+    expect(screen.getByText("myPasswordComponents")).toBeInTheDocument()
+});
+test('click prev', () => {
+    let mockFnSignIn = jest.fn(
+        () => {
+            return Promise.resolve();
+        })
+    useAuth.mockReturnValue({
+        isAuthenticated: false,
+        user: null,
+        signIn: mockFnSignIn
+    })
+
+    render(<Login/>);
+    userEvent.click(screen.getByText("Connexion"));
+
+    expect(mockFnSignIn).toHaveBeenCalled();
+})
+
