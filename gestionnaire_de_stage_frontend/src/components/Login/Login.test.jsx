c520839c85575bd77bd0fc0d81d515e89cfcfdd8
Samuel Bertrand <bertsa.pro@gmail.com>
2021-11-14 19:46:55 -0500

    Cleaning

--- gestionnaire_de_stage_frontend/src/components/Login/Login.test.jsx
+++ gestionnaire_de_stage_frontend/src/components/Login/Login.test.jsx
@@ -6,36 +6,31 @@ import userEvent from "@testing-library/user-event";
 import {useAuth} from "../../services/use-auth";
 
 
-
 jest.mock('../SharedComponents/Fields/FieldEmail', () => () => 'myEmailComponents');
 jest.mock('../SharedComponents/Fields/FieldPassword', () => () => 'myPasswordComponents');
 jest.mock('../../services/use-auth')
-beforeEach(() => {
-
-});
 
 afterEach(() => {
     cleanup();
     jest.resetAllMocks();
 })
-test('Login component', () => {
+test('Login component', async () => {
     render(<Login/>);
 
+    expect(await screen.findAllByRole('option')).toHaveLength(4)
     expect(screen.getByText("myEmailComponents")).toBeInTheDocument()
     expect(screen.getByText("myPasswordComponents")).toBeInTheDocument()
 });
-test('click prev', () => {
-    let mockFnSignIn = jest.fn(
-        () => {
-            return Promise.resolve();
-        })
+
+test('Login', () => {
+    let mockFnSignIn = jest.fn(() => Promise.resolve())
     useAuth.mockReturnValue({
         isAuthenticated: false,
         user: null,
         signIn: mockFnSignIn
     })
-
     render(<Login/>);
+
     userEvent.click(screen.getByText("Connexion"));
 
     expect(mockFnSignIn).toHaveBeenCalled();
