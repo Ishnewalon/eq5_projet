74f9415a2b91f60b7a08e3fbed136b2ebb03e0fb
Samuel Bertrand <bertsa.pro@gmail.com>
2021-11-17 12:58:34 -0500

    CardsView

--- gestionnaire_de_stage_frontend/src/components/ManagerView/RapportsView/RapportsView.jsx
+++ gestionnaire_de_stage_frontend/src/components/ManagerView/RapportsView/RapportsView.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function RapportsView() {
     return (<>
             <h1 className={"text-center"}>Rapports:</h1>
-            <div className="row">
+            <CardsView>
                 <ButtonBlock title="Rapport des offres validées"
                              description="Ma description"
                              to="/dashboard/rapports/1"/>
@@ -20,43 +20,43 @@ export default function RapportsView() {
                 <ButtonBlock title="Rapport des étudiants avec des Cv Invalides"
                              description="Ma description"
                              to="/dashboard/rapports/5"/>
-            </div>
+            </CardsView>
         </>
-
     )
 }
 
-// function ddd(props) {
-//     let leee
-//     let reactPortals = React.Children.toArray(props.children);
-//     for (let i = 0; i < reactPortals.length; i+=2) {
-//         let portal = reactPortals[i];
-//         if ()
-//         let portal2 = reactPortals[i+1];
-//         leee.push (<>
-//                 <div className="col-md-6">
-//                     {portal}
-//                 </div>
-//                 <div className="col-md-6">
-//                     {portal2}
-//                 </div>
-//             </>
-//         )
-//     }
-//
-//     }
-// }
+function CardsView(props) {
+    let myColumn = []
+    let myChildren = React.Children.toArray(props.children) || [];
+    let lastChild
+    if (myChildren.length % 2)
+        lastChild = myChildren.pop();
+    for (let i = 0; i < myChildren.length; i++)
+        myColumn.push(
+            <div className="col-sm-6" key={i}>
+                {myChildren[i]}
+            </div>)
+
+    if (lastChild)
+        myColumn.push(
+            <div className="col-sm-6 mx-auto" key={myChildren.length + 1}>
+                {lastChild}
+            </div>)
+
+    return (
+        <div className="row">
+            {myColumn}
+        </div>
+    )
+}
 
 function ButtonBlock({title, description, to}) {
     return (
-        <div className="col-sm-6">
-            <div className="card mb-3 bg-light">
-                <div className="card-body">
-                    <h5 className="card-title">{title}</h5>
-                    {/*<h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>*/}
-                    <p className="card-text">{description}</p>
-                    <Link className={"btn btn-primary btn-block float-end"} to={to}>Ouvrir</Link>
-                </div>
+        <div className="card mb-3 bg-light">
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+                <Link className={"btn btn-primary btn-block float-end"} to={to}>Ouvrir</Link>
             </div>
         </div>
     )
