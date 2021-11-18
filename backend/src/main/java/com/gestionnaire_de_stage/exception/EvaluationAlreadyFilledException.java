62e38d2a0da3b0137f553c0fbd66f81ca1aa0986
Stephanie <ishnewalon@hotmail.com>
2021-11-18 14:34:00 -0500

    Fixed EvaluationAlredyFilledException

--- backend/src/main/java/com/gestionnaire_de_stage/exception/EvaluationAlreadyFilledException.java
+++ backend/src/main/java/com/gestionnaire_de_stage/exception/EvaluationAlreadyFilledException.java
@@ -1,13 +1,8 @@
 package com.gestionnaire_de_stage.exception;
 
 public class EvaluationAlreadyFilledException extends Exception {
-    private final String message;
 
     public EvaluationAlreadyFilledException(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
+        super(message);
     }
 }
