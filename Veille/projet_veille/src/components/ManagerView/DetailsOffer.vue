edb26ca540ca2e97252f0b3954049ad785d922bd
Imem Belcadi Abbassi <belcadiimem01@gmail.com>
2021-11-23 11:51:39 -0500

    DetailsOffer.vue done

--- Veille/projet_veille/src/components/ManagerView/DetailsOffer.vue
+++ Veille/projet_veille/src/components/ManagerView/DetailsOffer.vue
@@ -1,20 +1,36 @@
 <template>
     <div>
-        <p>{{this.id}}</p>
+        <h1><i>{{ this.offer.title }}</i></h1>
+        <h2>{{ this.offer.description }}</h2>
+        <h2>L'offre commence le {{ this.offer.dateDebut }} et se termine {{ this.offer.dateFin }}</h2>
+        <router-link to="/ValidationOffers">
+            <button>Retour</button>
+        </router-link>
+
     </div>
 </template>
 
 <script>
+
 export default {
     name: "DetailsOffer",
     data() {
         return {
-            id: "",
+            offer: {},
         }
     },
+    methods: {
+        async getOffer() {
+            const response = await fetch(`http://localhost:8181/offers/get_offer/` + this.$route.query.prop, {
+                mode: 'cors',
+                method: "GET",
+            });
+            this.offer = await response.json();
+        },
+    },
     created() {
-        this.id = this.$route.query.prop;
-    }
+        this.getOffer();
+    },
 }
 </script>
 
