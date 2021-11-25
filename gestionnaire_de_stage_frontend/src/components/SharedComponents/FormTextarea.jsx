96a10c85f5712e437bed3e9950af202997b6fe88
Samuel Bertrand <bertsa.pro@gmail.com>
2021-11-25 17:18:31 -0500

    CreateOffer with hook form

---
+++ gestionnaire_de_stage_frontend/src/components/SharedComponents/FormTextarea.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+import {FormField} from "./FormField/FormField";
+
+export function FormTextarea(props) {
+    const {register, error, label, name, placeholder, validation} = props;
+    return <FormField htmlFor={name}>
+        <label>{label}</label>
+        <textarea name={name} placeholder={placeholder}
+                  className={error ? "border-danger" : ""}{...register(name, validation)}/>
+        {error && <span>{error.message}</span>}
+    </FormField>;
+}
+
+FormTextarea.propTypes = {
+    label: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    placeholder: PropTypes.string,
+    register: PropTypes.func.isRequired,
+    error: PropTypes.object,
+    validation: PropTypes.object.isRequired
+};
+FormTextarea.defaultProps = {
+    placeholder: "",
+};
