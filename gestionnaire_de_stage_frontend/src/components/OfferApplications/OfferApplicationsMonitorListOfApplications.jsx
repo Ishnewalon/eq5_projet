fc0afb2ae2b5d338730f6040d90bb92f90e37dc4
Samuel Bertrand <bertsa.pro@gmail.com>
2021-11-27 23:41:44 -0500

    Beautiful
    Co-authored-by: Imem Belcadi Abbassi <belcadiimem01@gmail.com>

--- gestionnaire_de_stage_frontend/src/components/OfferApplications/OfferApplicationsMonitorListOfApplications.jsx
+++ gestionnaire_de_stage_frontend/src/components/OfferApplications/OfferApplicationsMonitorListOfApplications.jsx
@@ -3,9 +3,10 @@ import {getAllApplicants} from '../../services/offerAppService';
 import {useAuth} from "../../hooks/use-auth";
 import {ContainerBox} from "../SharedComponents/ContainerBox/ContainerBox";
 import {downloadFile, toPdfBlob, uniqBy} from "../../utility";
-import {AiOutlineFile} from "react-icons/all";
+import {FiDownload} from "react-icons/all";
 import OfferView from "../Offers/OfferView";
 import MessageNothingToShow from "../SharedComponents/MessageNothingToShow/MessageNothingToShow";
+import {Table, TableHeader, TableRow} from "../SharedComponents/Table/Table";
 
 export default function OfferApplicationsMonitorListOfApplications() {
     let auth = useAuth();
@@ -25,34 +26,59 @@ export default function OfferApplicationsMonitorListOfApplications() {
         return <MessageNothingToShow message="Au moment, aucun étudiant n'a postulé à cette offre"/>
     }
 
-    return <>
-        <ContainerBox>
-            {offers.map((student, index) =>
-                <div key={index}>
-                    <PreviewStudent dto={student}/>
-                </div>)}
-        </ContainerBox>
-    </>;
-}
-
-function PreviewStudent({dto}) {
-
-    return <div className=" p-3 mt-5 border-left border-right border-light">
-        <div className={'row'}>
-            <div className="col-12 col-sm-6">
-                <div className={'d-flex justify-content-center align-items-center flex-column p-3 shadow h-100'}>
-                    <h4 className={'p-2 rounded fw-bold'}>{dto.firstName + ', ' + dto.lastName} </h4>
-                    <div className={'d-flex align-items-center'}>
-
-                        <button onClick={() => downloadFile(toPdfBlob(dto.file), dto.fileName)}
-                                className="ms-2 btn btn-primary"><AiOutlineFile/> {dto.fileName}
+    const getTableStudents = (index, offerApplications, offer) => {
+        let id = offer.id;
+        return <div className="card-footer">
+            <div className="accordion accordion-flush" id={"accordionFlushExample" + id}>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id={"flush-headingOne" + id}>
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target={"#flush-collapseOne" + id} aria-expanded="false"
+                                aria-controls="flush-collapseOne">
+                            Applications
                         </button>
+                    </h2>
+                    <div id={"flush-collapseOne" + id} className="accordion-collapse collapse"
+                         aria-labelledby="flush-headingOne" data-bs-parent={"#accordionFlushExample" + id}>
+                        <Table>
+                            <TableHeader>
+                                <th>Matricule</th>
+                                <th>Nom complet</th>
+                                <th>Email</th>
+                                <th>Téléphone</th>
+                                <th>Statut de l'application</th>
+                                <th>Curriculum</th>
+                            </TableHeader>
+                            {offerApplications.filter(offerApp => offerApp.offer.id === offer.id).map(offerApp => {
+                                    let curriculum = offerApp.curriculum;
+                                    let student = curriculum.student;
+                                    let lastName = student.lastName;
+                                    let firstName = student.firstName;
+                                    return <TableRow key={offerApp.id}>
+                                        <td>{student.matricule}</td>
+                                        <td>{`${lastName}, ${firstName}`}</td>
+                                        <td>{student.email}</td>
+                                        <td>{student.phone}</td>
+                                        <td>{offerApp.status}</td>
+                                        <td>
+                                            <button className="link-button"
+                                                    onClick={() => downloadFile(toPdfBlob(curriculum.data), `${firstName}_${lastName}_${curriculum.id}.pdf`)}>
+                                                <FiDownload/>
+                                            </button>
+                                        </td>
+                                    </TableRow>
+                                }
+                            )}
+                        </Table>
                     </div>
                 </div>
             </div>
-            <div className="col-12 col-sm-6">
-                <OfferView offer={dto.offerDTO}/>
-            </div>
         </div>
-    </div>
+    }
+    return <>
+        <ContainerBox>
+            {offers.map((offer, index) =>
+                <OfferView key={index} offer={offer} footers={getTableStudents(index, offerApplications, offer)}/>)}
+        </ContainerBox>
+    </>;
 }
