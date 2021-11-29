ba3754b91e8dfcb05a9faa5c33cebdd63dccd346
Jordan Scott <jordan.scott1396@gmail.com>
2021-11-28 23:28:56 -0500

    merge

--- backend/src/main/java/com/gestionnaire_de_stage/service/NotificationService.java
+++ backend/src/main/java/com/gestionnaire_de_stage/service/NotificationService.java
@@ -9,8 +9,6 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
