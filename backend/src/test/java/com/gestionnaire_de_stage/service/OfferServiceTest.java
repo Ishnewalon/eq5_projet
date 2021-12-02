fe8294e35bd0886d118c33f63512f97e9696969d
OussaKab <oussamakably@gmail.com>
2021-12-02 17:45:26 -0500

    fix tests

--- backend/src/test/java/com/gestionnaire_de_stage/service/OfferServiceTest.java
+++ backend/src/test/java/com/gestionnaire_de_stage/service/OfferServiceTest.java
@@ -213,23 +213,6 @@ public class OfferServiceTest {
     }
 
     @Test
-    public void testGetOffersNotAppliedYet() throws IdDoesNotExistException {
-        List<Offer> dummyOfferList = getDummyOfferList();
-        List<OfferApplication> dummyOfferApplicationList = getDummyOfferAppList();
-        Long studentId = 1L;
-        fixedClock = Clock.fixed(LocalDate.of(Year.now().getValue(), 1, 1).atStartOfDay(ZoneId.systemDefault()).toInstant(), ZoneId.systemDefault());
-        doReturn(fixedClock.instant()).when(clock).instant();
-        doReturn(fixedClock.getZone()).when(clock).getZone();
-        when(offerRepository.findAllByValidIsTrueAndSession_YearGreaterThanEqual(any())).thenReturn(dummyOfferList);
-        when(offerApplicationRepository.getAllByCurriculum_StudentId(any())).thenReturn(dummyOfferApplicationList);
-        when(studentRepository.existsById(any())).thenReturn(true);
-
-        List<Offer> actualOfferList = offerService.getOffersNotYetApplied(studentId);
-
-        assertThat(actualOfferList.size()).isEqualTo(2);
-    }
-
-    @Test
     public void testGetOffersNotAppliedYet_Summer() throws IdDoesNotExistException {
         List<Offer> dummyOfferList = getDummyOfferList();
         List<OfferApplication> dummyOfferApplicationList = getDummyOfferAppList();
